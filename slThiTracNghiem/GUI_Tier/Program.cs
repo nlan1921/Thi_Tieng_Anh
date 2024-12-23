505b2ba681f45745435fdbdb13f5d788c4d464e6
nlan1921 <an356039@gmail.com>
2024-12-23 12:05:18 +0700

    1
    1

---
+++ slThiTracNghiem/GUI_Tier/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GUI_Tier
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            frm3_ThiTracNghiem f = new frm3_ThiTracNghiem();
+            f.ShowDialog();
+            if (f.DialogResult == DialogResult.OK)
+            {
+                Application.Run(new frm2_GiaoDienChinh());
+            }
+            
+           
+        }
+    }
+}
---
+++ slThiTracNghiem/GUI_Tier/frm0_FormSpalsh.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace GUI_Tier
+{
+    partial class frm0_FormSpalsh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm0_FormSpalsh
+            // 
+            this.ClientSize = new System.Drawing.Size(978, 476);
+            this.Name = "frm0_FormSpalsh";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblFlash;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+
+    }
+}
